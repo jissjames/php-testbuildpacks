0c85dca8288847318951fb9bc5002a471140be66
jissjames <113381566@umail.ucc.ie>
2017-09-29 16:19:35 +0000

    Testing buildpack launch

---
+++ TestPHP2.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <title>TestPHP2</title>
+    </head>
+    
+    <body>
+        
+        <?php
+            echo "Welcome ".$_POST["name"]."<br/>";
+            echo "Your father is ".$_POST["fathername"];
+        ?>
+        
+    </body>
+</html>
